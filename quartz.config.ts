e11d701da7aab64a8f93082ebfd0eae805170bac
Luiz Fernando <luizfernandolfsg@gmail.com>
2025-08-21 00:54:24 +0200

    chore: configure GitHub Pages for Quartz (update baseUrl, add Pages workflow)

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "luizfernandosg.github.io/local-refi-toolkit",
+    baseUrl: "refidao.github.io/local-refi-toolkit",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
